618af48b08ebc4f2d698a627c06fb8154bad1e80
HMinng <jackhm18@gmail.com>
2014-12-16 10:53:26 +0800

    changed

--- Statistics.sh
+++ Statistics.sh
@@ -4,15 +4,18 @@
 
 
 g=./result/tt/g.tt
-h=./result/tt/h.txt
+h=./result/tt/h.tt
+dict=./result/tt/dict1.tt
 
 cat $g | while read line;
 do
     search=$(echo -n "$line" | awk '{print $2}');
     
-    num=$(echo -n "$line" | awk '{print $1}');
+    exists=$(cat $dict | grep $earch | wc -l);
 
-    content=$(cat $g | grep $search | tail -n 1 | awk -F'\t' '{print $5}');
-    
-    echo -e "$num\t$content" >> statistics.tt;
+    if [ $exists -eq 0 ]; then
+        num=$(echo -n "$line" | awk '{print $1}');
+        content=$(cat $h | grep $search | tail -n 1 | awk -F'\t' '{print $5}');
+        echo -e "$num\t$content" >> statistics.tt;
+    fi
 done
